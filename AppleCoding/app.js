b3961b777da64592bb3010a8549fdacf33229fb5
Vivek Srinivasa <vivekdars@gmail.com>
2017-03-30 18:12:14 -0400

    Add files via upload

---
+++ AppleCoding/app.js
@@ -0,0 +1,21 @@
+/**
+ * Created by vivek on 3/30/2017.
+ */
+// create angular app
+var validationApp = angular.module('validationApp', []);
+
+// create angular controller
+validationApp.controller('mainController', function($scope) {
+
+    // function to submit the form after all validation has occurred
+    $scope.submitForm = function() {
+
+        // check to make sure the form is completely valid
+        if ($scope.userForm.$valid) {
+            alert('our form is amazing');
+        }
+
+    };
+
+});
+
